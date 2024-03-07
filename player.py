f0dcc5183f530cde37f2fbbcd6f6f659df0f278f
Meenu Gupta0007 <findmeenu@gmail.com>
2024-03-07 10:48:39 +0100

    Best

--- player.py
+++ player.py
@@ -4,21 +4,19 @@ class Player:
         self.name = name 
         self.score = 0
         self.tempscore = 0
-        self.filename = 'players.txt'
         self.wins = 0 
         self.loss = 0 
         self.count_of_games_played = 0  
               
-    def score1(self, roll_dice_num):
-                
+    def score1(self, roll_dice_num):        
         if roll_dice_num == 1:  # When the player rolled 1
             self.tempscore = 0 
-        elif roll_dice_num == 0: # When the player selected to hold            
-            self.score = self.tempscore
+        elif roll_dice_num == 0:   # When the player selected to hold            
+            self.score += self.tempscore
             self.tempscore = 0
-        else: # When the player rolled 2 to 6
+        else:   # When the player rolled 2 to 6
             self.tempscore += roll_dice_num
-            if self.tempscore + self.score >=30:
+            if self.tempscore + self.score > 30:
                 self.score += self.tempscore
     
     def won(self):
